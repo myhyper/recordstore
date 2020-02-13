9d556d02c9c043424907081873ab06aba11e36d0
myhyper <myeongje@gmail.com>
2020-02-13 17:51:19 +0900

    Mistypo Fixed

--- recordstore-back/app/controllers/refresh_controller.rb
+++ recordstore-back/app/controllers/refresh_controller.rb
@@ -3,7 +3,7 @@ class RefreshController < ApplicationController
 
   def create
     session = JWTSessions::Session.new(payload: claimless_payload, refresh_by_access_allowed: true)
-    tokens = session.refresh_by_access_allowed1 do
+    tokens = session.refresh_by_access_allowed do
       raise JWTSession::Errors::Unauthorized, "Somethings not rijght here!"
     end
 
