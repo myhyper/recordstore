909dacf68a1e1f9dea68d8a03e286ced289a1d14
Max Park <myeongje@gmail.com>
2020-02-28 16:02:44 +0900

    Vue Starter init

---
+++ vue-starter/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Navbar/>
+    <h2>App</h2>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar'
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import '../node_modules/materialize-css/dist/js/materialize.min.js';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ vue-starter/src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Vue lol</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#">home</a></li>
+        <li><a href="#">about</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
