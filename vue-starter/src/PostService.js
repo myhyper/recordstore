2cd480e356e369578f4b5166b68e0acbe31e25e0
Max Park <myeongje@gmail.com>
2020-02-29 16:40:56 +0900

    Sync

--- vue-starter/src/PostService.js
+++ vue-starter/src/PostService.js
@@ -4,6 +4,6 @@ const apiBaseUrl = 'http://ndb99xkpdk.execute-api.eu-west-2.amazonaws.com/dev';
 
 export default class PostService {
     getAllPosts() {
-        return axios.get(`${apiBaseUrl}/posts`);
+        return axios.get(`${apiBaseUrl}/posts`, {redir_token:'4qF5mh6Qf4NgSNsbvj_Dk-DkFdJ8MTU4Mjk3NzYwN0AxNTgyODkxMjA3&v=VEhFLM8tw0o'});
     }
 }
